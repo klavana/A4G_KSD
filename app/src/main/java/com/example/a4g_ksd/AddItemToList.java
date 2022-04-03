a566da34e3939ed0c01c38b57159aa3650fb371f
davidzhukovsky <zhukovsky.david@gmail.com>
2022-04-03 11:09:39 -0400

    4/3/2022 Code not working :(  Uploaded so team can help :) Basically, AddItemsToList and UserOffers need to see the same User but they dont.  Mrs. Taricco reccomended using a Controller class, which I have partially set up.  For reference, trivia tutorial at 3:20 discusses Controller classes.  LMK if questions

--- app/src/main/java/com/example/a4g_ksd/AddItemToList.java
+++ app/src/main/java/com/example/a4g_ksd/AddItemToList.java
@@ -7,12 +7,22 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class AddItemToList extends AppCompatActivity {
+    TextView editItemQuantity = findViewById(R.id.editItemQuantity);
+    String Quantity = editItemQuantity.getText().toString();
+    int quant = Integer.parseInt(Quantity);
+    TextView EditItemName = findViewById(R.id.EditItemName);
+    String Name = editItemQuantity.getText().toString();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.additemtolist);
         Intent intent = getIntent();
-
+    }
+    public void addOffer(String name, int Quantity){
+        Item i = new Item(Name, quant);
+        final Controller aController = (Controller) getApplicationContext();
+        aController.getUser().addItem(i);
     }
 }
