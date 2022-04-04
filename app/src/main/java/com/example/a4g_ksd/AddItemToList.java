e6e320728295bc592dfe82b262fb63574dbc79e0
karismalavana <klavana@wpi.edu>
2022-04-03 21:18:45 -0400

    add item issue resolved

--- app/src/main/java/com/example/a4g_ksd/AddItemToList.java
+++ app/src/main/java/com/example/a4g_ksd/AddItemToList.java
@@ -4,25 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddItemToList extends AppCompatActivity {
-    TextView editItemQuantity = findViewById(R.id.editItemQuantity);
-    String Quantity = editItemQuantity.getText().toString();
-    int quant = Integer.parseInt(Quantity);
-    TextView EditItemName = findViewById(R.id.EditItemName);
-    String Name = editItemQuantity.getText().toString();
 
+    //Data
+    EditText editItemQuantity, editItemName;
+    String quantityStr;
+    int quantity;
+    String name;
 
     @Override
+    //"Constructor"
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.additemtolist);
+        // must be initialized in constructor
+        editItemQuantity= findViewById(R.id.editItemQuantity);
+        editItemName = findViewById(R.id.EditItemName);
         Intent intent = getIntent();
+        Toast.makeText(getApplicationContext(),"Add Item to List Working",Toast.LENGTH_SHORT).show();
     }
-    public void addOffer(String name, int Quantity){
-        Item i = new Item(Name, quant);
-        final Controller aController = (Controller) getApplicationContext();
-        aController.getUser().addItem(i);
+
+    // Methods
+    public void addOffer(View v){
+        quantityStr = editItemQuantity.getText().toString();
+        quantity = Integer.parseInt(quantityStr);
+        name = editItemQuantity.getText().toString();
+        Item i = new Item(name, quantity);
+        final Controller controller = (Controller) getApplicationContext();
+        controller.getUser().addItem(i);
+        Toast.makeText(getApplicationContext(),"Add Offer",Toast.LENGTH_SHORT).show();
+
     }
 }
